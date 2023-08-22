f6c4a8665ed7e5f0216de1299b2a7bc981b9f1af
Shane Hingtgen <shanehingtgen@gmail.com>
2023-08-21 21:53:18 -0500

    backing up
    added GET and POST API with error messages, added an example of a production config file

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -9,6 +9,25 @@
 const express = require("express");
 const router = express.Router();
 const { mongo } = require("../utils/mongo");
+const Ajv = require("ajv");
+const { ObjectId } = require("mongodb");
+
+const ajv = new Ajv(); //creates a new instance of Ajv class
+
+// define a scheme to validate a new task
+// TODO: Figure out why this is not preventing additional properties
+// TODO: as of now it is allowing us to send a second property of bar.
+
+const taskSchema = {
+  type: "object",
+  properties: {
+    text: {
+      type: "string",
+    },
+  },
+  required: ["text"],
+  additionalProperties: false,
+};
 
 /**
  * findEmployeeById
@@ -42,6 +61,7 @@ router.get("/:empId", (req, res, next) => {
     let { empId } = req.params; //get the empId from the req.params object
     empId = parseInt(empId, 10); // try to determine if empId is numerical value
 
+    //an early return method
     if (isNaN(empId)) {
       // if empId is not a number
       const err = new Error("input must be a number");
@@ -55,6 +75,7 @@ router.get("/:empId", (req, res, next) => {
       async (db) => {
         const employee = await db.collection("employees").findOne({ empId }); //find employee by ID
 
+        //another early return method
         if (!employee) {
           // if empId does not exist
           const err = new Error("Unable to find employee with empId ", empId);
@@ -75,4 +96,119 @@ router.get("/:empId", (req, res, next) => {
   }
 });
 
+/**
+ * findAllTasks
+ * @openapi
+ */
+router.get("/:empId/tasks", (req, res, next) => {
+  try {
+    console.log("findAllTasks API");
+    let { empId } = req.params; //get the empId from the req.params object
+    empId = parseInt(empId, 10); // try to determine if empId is numerical value
+
+    if (isNaN(empId)) {
+      const err = new Error("input must be a number");
+      err.status = 400;
+      console.log("err", err);
+      next(err);
+      return;
+    }
+
+    mongo(async (db) => {
+      const tasks = await db
+        .collection("employees")
+        .findOne({ empId }, { projection: { empId: 1, todo: 1, done: 1 } });
+      console.log("tasks", tasks);
+
+      if (!tasks) {
+        const err = new Error("unable to find tasks for empId ", empId);
+        err.status = 404;
+        console.log("err", err);
+        next(err);
+        return;
+      }
+
+      res.send(tasks); //return the tasks array
+    }, next);
+  } catch (err) {
+    console.log("err", err);
+    next(err);
+  }
+});
+
+/**
+ *
+ */
+
+router.post("/:empId/tasks", (req, res, next) => {
+  try {
+    console.log("createTask API");
+
+    let { empId } = req.params; //get the empId from the req.params object
+    empId = parseInt(empId, 10); // try to determine if empId is numerical value
+
+    if (isNaN(empId)) {
+      const err = new Error("input must be a number");
+      err.status = 400;
+      console.log("err", err);
+      next(err);
+      return;
+    }
+
+    mongo(async (db) => {
+      const employee = await db.collection("employees").findOne({ empId });
+
+      console.log("employee", employee);
+
+      if (!employee) {
+        const err = new Error("unable to find employee with empId ", empId);
+        err.status = 404;
+        console.log("err", err);
+        return;
+      }
+
+      const { text } = req.body;
+      console.log("req.body", req.body);
+
+      // validate the req object
+      const validator = ajv.compile(taskSchema);
+      const valid = validator({ text });
+
+      console.log("valid", valid);
+
+      if (!valid) {
+        const err = new Error("Bad Request");
+        err.status = 400;
+        err.errors = validator.errors;
+        console.log("req.body validation failed", err);
+        next(err);
+        return;
+      }
+
+      const task = {
+        _id: new ObjectId(),
+        text,
+      };
+
+      const result = await db
+        .collection("employees")
+        .updateOne({ empId }, { $push: { todo: task } });
+
+      console.log("result", result);
+
+      if (!result.modifiedCount) {
+        const err = new Error("unable to create task for empId ", empId);
+        err.status = 404;
+        console.log("err", err);
+        next(err);
+        return;
+      }
+
+      res.status(201).send({ id: task._id });
+    }, next);
+  } catch (err) {
+    console.log("err", err);
+    next(err);
+  }
+});
 module.exports = router;
---
+++ server/utils/config.js
@@ -0,0 +1,15 @@
+// production example
+"use strict";
+
+const {
+  DB_USERNAME = "nodebucket_user",
+  DB_PASSWORD = "s3cret",
+  DB_NAME = "nodebucket",
+} = process.env;
+
+const CONFIG = {
+  DB_URL: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@bellevueuniversity.ut5xprd.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+  DB_NAME: DB_NAME,
+};
+
+module.exports = CONFIG;
