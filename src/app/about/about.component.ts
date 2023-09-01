bc5c7fb3e70e837795eee933cdc9a9005d31f069
Shane Hingtgen <shanehingtgen@gmail.com>
2023-08-31 21:59:53 -0500

    backing up sprint 3
    fixed API swagger doc issue, was missing the entire path
    fixed functional tests for each sprint
    added swagger docs for sprint 3 to tdd
    added soapui tests for sprint 3 to tdd
    themeing
    about page got some nice backstory and bios and photos
    404 page
    landing page got better images
    code attribution

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,14 @@
+/**
+ * Title: about.component.ts
+ * Author: Shane Hingtgen
+ * Date: 8/31/23
+ */
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,10 +1,15 @@
+/**
+ * Title: nav.component.ts
+ * Author: Professor Krasso
+ * Modified by: Shane Hingtgen
+ * Date: 8/31/23
+ */
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
